19c39e4af8e499ba1dcfebb92c0da8cd127debd4
steveanlorn <steve.anlorn@gmail.com>
2021-07-05 22:51:04 +0700

    day 39

---
+++ day_39/main.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+
+import os
+
+SHEETY_EP = os.environ.get("SHEETY_EP")
+SHEETY_AUTH_USERNAME = os.environ.get("SHEETY_AUTH_USERNAME")
+SHEETY_AUTH_PASSWORD = os.environ.get("SHEETY_AUTH_PASSWORD")
+data_manager = DataManager(SHEETY_EP, SHEETY_AUTH_USERNAME, SHEETY_AUTH_PASSWORD)
+
+TEQUILA_KEY = os.environ.get("TEQUILA_KEY")
+flight_search = FlightSearch(TEQUILA_KEY)
+
+sheet_data = data_manager.get_destination_data()
+
+ORIGIN_CITY_IATA = "JKT"
+
+if sheet_data[0]["iataCode"] == "":
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+
+    if flight.price < destination["lowestPrice"] or destination["lowestPrice"] == 0:
+        message = f"Low price alert! Only Rp{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        print(message)
